030e16e25a80c0f5069e7a6b7b172f1b0ff7185f
StellaTeuscher <stella.teuscher@gmail.com>
2025-04-01 14:02:59 -0400

    finished logic of project

--- controllers/community.js
+++ controllers/community.js
@@ -15,11 +15,25 @@ router.get("/", async (req, res) => {
 
 router.get("/show/:userId", async (req, res) => {
     try {
-        const user = await User.findById(req.params.userId);
-        res.render("community/show.ejs", { user });
+        const user = await User.findById(req.session.user._id);
+        // watchedUser is the other user which we're looking at
+        const watchedUser = await User.findById(req.params.userId);
+        res.render("community/show.ejs", { watchedUser, user });
     } catch (error){
         console.log(error);
         res.redirect("/");
+    }
+})
+
+router.get("/show/:userId/:itemId",async (req, res) => {
+    try {
+        const user = await User.findById(req.session.user._id);
+        const watchedUser = await User.findById(req.params.userId);
+        const item = watchedUser.wishlist.id(req.params.itemId);
+        res.render("community/item-details.ejs", { watchedUser, user, item });
+    } catch (error) {
+        console.log(error);
+        res.redirect("/")
     }
 })
 
